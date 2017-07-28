c5e2d3681be5f4619faf41e5ac52913ce046b1c1
Makoto Inoue <makoto.inoue@simplybusiness.co.uk>
2017-07-28 13:45:44 +0100

    fix broken test

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,9 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+var MerdaCrowdsale = artifacts.require("./MerdeCrowdsale.sol")
 
-module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
-};
+module.exports = function(deployer, network, accounts) {
+  const startBlock = web3.eth.blockNumber + 5 // blockchain block number where the crowdsale will commence. Here I just taking the current block that the contract and setting that the crowdsale starts two block after
+  const endBlock = startBlock + 300  // blockchain block number where it will end. 300 is little over an hour.
+  const rate = new web3.BigNumber(1000) // rate of ether to Gustavo Coin in wei
+  const wallet = web3.eth.accounts[0] // the address that will hold the fund. Recommended to use a multisig one for security.
+  deployer.deploy(MerdaCrowdsale, startBlock, endBlock, rate, wallet)
+}
